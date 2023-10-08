bee797bc92af397fbc44872e207b7948c9f209de
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-10-08 19:45:01 +0700

    update

--- test/platform_selector_test.dart
+++ test/platform_selector_test.dart
@@ -4,27 +4,18 @@
 /// @TestOn() digunakan untuk menentukan platform yang digunakan untuk menjalankan test case
 /// Untuk lebih jelasnya bisa cek dokumentasi @TestOn() di https://pub.dev/documentation/test/latest/test/TestOn-class.html
 /// Platform selector mendukung operator && dan || untuk menentukan platform yang digunakan
-/// Platform yang bisa digunakan untuk @TestOn() adalah: VM, Chrome, Node.JS, Content Shell, PhantomJS, Firefox, Safari, dan IE
+/// Platform yang bisa digunakan untuk: VM, Chrome, Node.JS, Content Shell, PhantomJS, Firefox, Safari, dan IE, Android, iOS, Windows, dan MacOS
+/// Selain itu bisa juga menggunakan nama platform yang tidak terdaftar
+/// Untuk lebih jelasnya bisa cek dokumentasi platform selector di https://pub.dev/documentation/test/latest/test/TestOn-class.html
+
+
+// example platform selector test
+@TestOn("windows || mac-os") // global platform selector test for all test case
 
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
-import 'package:test/test.dart';
 
 String sayHello(String name) => 'Hello $name!'; // function
-
-// example function dayOfWeek
-String dayOfWeek(int day) {
-  switch (day) {
-    case 1:
-      return "Monday";
-    case 2:
-      return "Tuesday";
-    case 3:
-      return "Wednesday";
-    default:
-      return "Unknown";
-  }
-}
 
 void main(){
   group("sayHello() function test", (){ // group test case
@@ -35,18 +26,6 @@ void main(){
     test("sayHello function test", (){ // test case
       var greeting = sayHello("Bob"); // function call
       expect(greeting, "Hello Bob!"); // assertion
-    });
-  });
-
-  group("dayOfWeek() function test", (){ // group test case
-
-    test("dayOfWeek() function test", (){ // test case
-      expect(dayOfWeek(1), "Monday"); // assertion
-    });
-
-    test("dayOfWeek function test", (){ // test case
-      var day = dayOfWeek(1); // function call
-      expect(day, "Monday"); // assertion
-    });
+    }, testOn: "windows"); // test only on windows platform
   });
 }
