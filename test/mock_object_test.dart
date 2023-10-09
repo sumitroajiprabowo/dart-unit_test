b2a655423c1880563b4945ecb793473603e92c2d
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-10-10 00:10:12 +0700

    update

--- test/mock_object_test.dart
+++ test/mock_object_test.dart
@@ -34,7 +34,7 @@ void main() {
           1, "Dart Programming", "Bob", 2021))).called(1);
     });
 
-    test("getBook() function test", () {
+    test("getBookByID() function test", () {
       when(bookRepository.getBookByID(Book(1, "", "", 0)))
           .thenReturn(Book(1, "Dart Programming", "Bob", 2021));
       bookService.getBookByID(1);
@@ -43,5 +43,36 @@ void main() {
       verify(bookRepository.getBookByID(Book(1, "", "", 0))).called(3);
     });
 
+    test("getBookByID() with Matcher", () {
+      when(bookRepository.getBookByID(any))
+          .thenReturn(Book(1, "Dart Programming", "Bob", 2021));
+      bookService.getBookByID(1);
+      expect(bookRepository.getBookByID(Book(1, "", "", 0)),
+          Book(1, "Dart Programming", "Bob", 2021));
+      verify(bookRepository.getBookByID(Book(1, "", "", 0))).called(2);
+    });
+
+    test("getBookByID() with Argument Matcher", () {
+      when(bookRepository.getBookByID(argThat(
+          predicate<Book>((book) => book.id == 333))))
+          .thenReturn(Book(333, "Dart Programming", "Bob", 2021));
+      bookService.getBookByID(1);
+      expect(bookRepository.getBookByID(Book(333, "", "", 0)),
+          Book(333, "Dart Programming", "Bob", 2021));
+      verify(bookRepository.getBookByID(Book(333, "", "", 0))).called(1);
+    });
+
+    test("deleteBook() function test", () {
+      when(bookRepository.getBookByID(Book(1, "", "", 0)))
+          .thenReturn(Book(1, "Dart Programming", "Bob", 2021));
+      bookService.deleteBook(1);
+      verify(bookRepository.deleteBook(Book(
+          1, "Dart Programming", "Bob", 2021))).called(1);
+    });
+
+    test("getAllBooks() function test", () {
+      bookService.getAllBooks();
+      verify(bookRepository.getAllBooks()).called(1);
+    });
   });
 }
